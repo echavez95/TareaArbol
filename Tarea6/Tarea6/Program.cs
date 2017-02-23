e83b714093e7777198a79f95ab3a4c290e1f2fcf
echavez95 <eliezer.chavez@unitec.edu>
2017-02-23 17:16:29 -0600

    agregando ejemplo de recorrido de arboles

---
+++ Tarea6/Tarea6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.DirectoryInfo rootDir = new System.IO.DirectoryInfo("C:/Users/echavez/Desktop");
+            WalkDirectoryTree(rootDir);
+            Console.ReadKey();
+        }
+
+        static void WalkDirectoryTree(System.IO.DirectoryInfo root)
+        {
+            System.IO.DirectoryInfo[] subDirs = null;
+            subDirs = root.GetDirectories();
+            foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+            {
+                Console.WriteLine(dirInfo.Name);
+                // Resursive call for each subdirectory.
+                WalkDirectoryTree(dirInfo);
+            }
+        }
+    }
+}
